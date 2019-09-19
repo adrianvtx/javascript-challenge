9bae8f4d8458cd09f03b1ff98547b344e0a33f55
adrianvtx <adrian.voj.2019@gmail.com>
2019-09-18 19:17:27 -0500

    getting through Part 1

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -13,27 +13,25 @@ var tableData = data;
 // mongoimport --db data; --collection inventory --authenticationDatabase admin - username < admin > --password < admin > --drop --file ~\static\data.js;
 
 //                      <input class="form-control" id="datetime"
-button.on("click", () => {
-  d3.event.preventDefault();
-  var inputDateId = d3.select('#datetime');
-  var inputDateVal = inputDateId.property("value");
-  console.log(inputDateVal);
+  // event.preventDefault();
+// d3.selectAll('')
+var dateBut = d3.select('#filter-btn');
 
-  tableData.filter(item => item.datetime === inputDateVal);
+dateBut.on("click", () => {
+var inputDateId = d3.select("#datetime");
+var inputDateVal = inputDateId.property("value");
+console.log(inputDateVal);
 
-  // ********************************
-  // Get a reference to the table body
+var filteredData = tableData.filter(item => item.datetime === inputDateVal);
+console.log(filteredData);
 
+var tbody = d3.select("tbody");
 
-  console.log(tableData);
-  // ********************************************
-  var tbody = d3.select("tbody");
-
-
-  data.forEach(tableData => {
-    var row = tbody.append("tr");
-    Object.entries(tableData).forEach(([value]) => {
-      var cell = row.append("td");
-      cell.text(value);
-    });
-})
+data.forEach(filteredData => {
+  var row = tbody.append("tr");
+  Object.entries(filteredData).forEach(([key, value]) => {
+    var cell = row.append("td");
+    cell.text(value);
+  });
+ });
+});
