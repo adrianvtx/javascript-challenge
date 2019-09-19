1c520dc638bdfe8a23c5c90cbfee59743e707f20
adrianvtx <adrian.voj.2019@gmail.com>
2019-09-19 08:22:06 -0500

    Progress2

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -8,24 +8,40 @@ console.log(tableData);
 // event.preventDefault();
 d3.selectAll();
 var dateBut = d3.select('#filter-btn');
+
 var inputDateId = d3.select("#datetime");
-var inputDateVal = inputDateId.property("value").trim();
+// var inputDateVal = inputDateId.property("value").trim();
+var inputDateVal = inputDateId.text();
+
 console.log(inputDateVal);
 
-var tbody = d3.select("tbody");
 
 
-dateBut.on("click", () => {
-  "use strict";
+d3.select("#filter-btn").on("click", function() {
+  ("use strict");
+  d3.event.preventDefault();
+  var tbody = d3.select("tbody");
   tbody.html("");
   var filteredData = data.filter(data => data.datetime === inputDateVal);
-  console.log(filteredData);
-
   data.forEach(filteredData => {
-  var row = tbody.append("tr");
-  Object.entries(filteredData).forEach(([key, value]) => {
-  var cell = row.append("td");
-  cell.text(value);
-  });
+    console.log(filteredData);
+    var row = tbody.append(`tr`);
+    Object.entries(filteredData).forEach(([key, value]) => {
+      var cell = row.append(`td`);
+      cell.text(value);
+    });
   });
 });
+
+d3.select("#table-area")
+  .datum(data)
+  .call(myTable);
+// var tbody = d3.select("tbody");
+// data.forEach(tableData => {
+//   console.log(tableData);
+//   var row = tbody.append("tr");
+//   Object.entries(tableData).forEach(([key, value]) => {
+//     var cell = row.append("td");
+//     cell.text(value);
+//   });
+// });
