e67ecf07573df63611e739d4002ce4f07d26167f
adrianvtx <adrian.voj.2019@gmail.com>
2019-09-20 11:13:46 -0500

    progress 3

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -6,7 +6,8 @@ console.log(tableData);
 
 //                      <input class="form-control" id="datetime"
 // event.preventDefault();
-d3.selectAll();
+
+
 var dateBut = d3.select('#filter-btn');
 
 var inputDateId = d3.select("#datetime");
@@ -15,27 +16,42 @@ var inputDateVal = inputDateId.text();
 
 console.log(inputDateVal);
 
+// Submit Button handler
+function handleSubmit() {
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+
+    // Select the input value from the form
+    var ufoTable = d3.select("#ufo-table").node().value;
+    console.log(ufoTable);
+
+    // clear the input value
+    d3.select("#ufo-table").node().value = "";
+
+    // Build the plot with the new stock
+    buildPlot(ufoTable);
+}
 
 
 d3.select("#filter-btn").on("click", function() {
-  ("use strict");
-  d3.event.preventDefault();
-  var tbody = d3.select("tbody");
-  tbody.html("");
-  var filteredData = data.filter(data => data.datetime === inputDateVal);
-  data.forEach(filteredData => {
-    console.log(filteredData);
-    var row = tbody.append(`tr`);
-    Object.entries(filteredData).forEach(([key, value]) => {
-      var cell = row.append(`td`);
-      cell.text(value);
+    ("use strict");
+    d3.event.preventDefault();
+    var tbody = d3.select("tbody");
+    tbody.html("");
+    var filteredData = data.filter((data) => data.datetime === inputDateVal);
+    data.forEach(filteredData => {
+        console.log(filteredData);
+        var row = tbody.append(`tr`);
+        Object.entries(filteredData).forEach(([key, value]) => {
+            var cell = row.append(`td`);
+            cell.text(value);
+        });
     });
-  });
 });
 
 d3.select("#table-area")
-  .datum(data)
-  .call(myTable);
+    .datum(data)
+    .call(myTable);
 // var tbody = d3.select("tbody");
 // data.forEach(tableData => {
 //   console.log(tableData);
