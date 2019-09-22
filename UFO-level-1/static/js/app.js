1989ee45821964edfe656598501f3640ce187f5c
adrianvtx <adrian.voj.2019@gmail.com>
2019-09-22 10:13:39 -0500

    Progress 6

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -18,46 +18,24 @@ var tableData = data;
 // }
 
 
-function fillTable() {
-    var tbody = d3.select("tbody");
-    data.forEach(tableData) => {
-        // console.log(tableData);
-        var row = tbody.append("tr");
-        Object.entries(tableData).forEach(([key, value]) => {
-            var cell = row.append("td");
-            cell.text(value);
-        });
-    });
-}
-fillTable();
-
-var dateBut = d3.select('#filter-btn');
-
-var inputDateId = d3.select("#datetime").node().value;
-var compValue1 = inputDateId;
-
-// // from 08  stock act and avSnippets
-// function getData() {
-//     (function() {
-//         var datetime = unpack((0), 0);
-//         var city = unpack(data(0), 1);
-//         var state = unpack(data(0), 2);
-//         var country = unpack(data(0), 3);
-//         var shape = unpack(data(0), 4);
-//         var durationMinutes = unpack(data(0), 5);
-//         var comments = unpack(data(0), 6);
-//         buildTable(
-//             datetime,
-//             city,
-//             state,
-//             country,
-//             shape,
-//             durationMinutes,
-//             comments
-//         );
+// function buildTable() {
+//     var tbody = d3.select("tbody");
+//     data.forEach(tableData => {
+//         // console.log(tableData);
+//         var row = tbody.append("tr");
+//         Object.entries(tableData).forEach(([key, value]) => {
+//             var cell = row.append("td");
+//             cell.text(value);
+//         });
 //     });
 // }
 
+// fillTable();
+
+// var dateBut = d3.select('#filter-btn');
+
+var inputDateId = d3.select("#datetime").node().value;
+var compValue1 = inputDateId;
 
 // d3.select("#filter-btn").on("click", function() {
 //     ("use strict");
@@ -66,7 +44,7 @@ var compValue1 = inputDateId;
 //     tbody.html("");
 //     var filterArray = [s => s.datetime === compValue1];
 //     filterArray.forEach(f => (tableData = tableData.filter(f)));
-//     // console.log(filterArray);
+//     console.log(filterArray);
 //     // console.log(compValue1);
 //     // console.log(tableData);
 //     var row = tbody.append(`tr`);
@@ -122,29 +100,54 @@ function handleSubmit() {
     buildTable(ufoTable);
 }
 
+// handleSubmit();
+
 // d3.select("#ufo-table")
 //     .datum(data)
 //     .call(myTable);
-var tbody = d3.select("tbody");
-data.forEach(tableData => {
-    console.log(tableData);
-    var row = tbody.append("tr");
-    Object.entries(tableData).forEach(([key, value]) => {
-        var cell = row.append("td");
-        cell.text(value);
-    });
-});
+// var tbody = d3.select("tbody");
+// data.forEach(tableData => {
+//     console.log(tableData);
+//     var row = tbody.append("tr");
+//     Object.entries(tableData).forEach(([key, value]) => {
+//         var cell = row.append("td");
+//         cell.text(value);
+//     });
+// });
 
-body = document.getElementsByTagName("body")[0];
-tab = document.createElement("table");
-for (row = 0; row < tableData.length; row++) {
-    tr = document.createElement("tr");
-    for (col = 0; col < tableData[row].length; col++) {
-        td = document.createElement("td");
-        tn = document.createTextNode(arr[row][col]);
-        td.appendChild(tn);
-        tr.appendChild(td);
-    }
-    tab.appendChild(tr);
-}
-body.appendChild(tab);
+// body = document.getElementsByTagName("body")[0];
+// tab = document.createElement("table");
+// for (row = 0; row < tableData.length; row++) {
+//     tr = document.createElement("tr");
+//     for (col = 0; col < tableData[row].length; col++) {
+//         td = document.createElement("td");
+//         tn = document.createTextNode(arr[row][col]);
+//         td.appendChild(tn);
+//         tr.appendChild(td);
+//     }
+//     tab.appendChild(tr);
+// }
+// body.appendChild(tab);
+
+
+// // from 08  stock act and avSnippets
+// function getData() {
+//     (function() {
+//         var datetime = unpack((0), 0);
+//         var city = unpack(data(0), 1);
+//         var state = unpack(data(0), 2);
+//         var country = unpack(data(0), 3);
+//         var shape = unpack(data(0), 4);
+//         var durationMinutes = unpack(data(0), 5);
+//         var comments = unpack(data(0), 6);
+//         buildTable(
+//             datetime,
+//             city,
+//             state,
+//             country,
+//             shape,
+//             durationMinutes,
+//             comments
+//         );
+//     });
+// }
